9a02536605acefcca8f872e70f95344bae3ef6df
ziv katzav <zkatzav94@gmail.com>
2019-12-17 18:36:18 +0200

    setup files

---
+++ README.md
@@ -0,0 +1,5 @@
+CoaChange is a platform for trainers to find a replacement trainer / find a casual job in training.
+
+Front End - React.
+Back End - Firebase
+
---
+++ src/components/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+      <h2>Welcome to CoaChange!</h2>
+      <p>
+        CoaChange is a platform for trainers to find a replacement trainer / find a casual job in training.
+      </p>
+      <p>
+        Sign up now and you can post an ad / find a job.
+      </p>
+    </div>
+  )
+}
+
+export default Landing
